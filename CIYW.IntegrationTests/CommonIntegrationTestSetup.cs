529e8f3fd4d2446b9101b4c9f526d90835f58431
Oleksandr <130494310+oleksandramarty@users.noreply.github.com>
2024-01-03 22:42:00 +0200

    usings

--- CIYW.IntegrationTests/CommonIntegrationTestSetup.cs
+++ CIYW.IntegrationTests/CommonIntegrationTestSetup.cs
@@ -1,26 +1,4 @@
-﻿using System.Security.Claims;
-using Autofac;
-using Autofac.Extensions.DependencyInjection;
-using CIYW.Auth;
-using CIYW.Domain;
-using CIYW.Domain.Initialization;
-using CIYW.Domain.Models.User;
-using CIYW.Interfaces;
-using CIYW.Kernel.Extensions;
-using CIYW.Mediator;
-using CIYW.Repositories;
-using CIYW.UnitTests;
-using CYIW.Mapper;
-using MediatR;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Diagnostics;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
-using NUnit.Framework;
-using Xunit;
+﻿using NUnit.Framework;
 
 namespace CIYW.IntegrationTests;
 
